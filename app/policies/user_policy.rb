8cc3a21f6f7629376ea93d06a1b710c71ed72e8e
Tony Blum <tonyblum@me.com>
2016-03-10 10:30:32 -0800

    add application controller with pundit setup

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,12 @@
+class UserPolicy
+  attr_reader :cuurent_user, :model
+
+  def initialize(current_user, model)
+    @current_user = current_user
+    @user = model
+  end
+
+  def index?
+    @current_user.admin?
+  end
+end
